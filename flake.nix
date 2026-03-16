0b3fe31ecc37be3c13aaae680cf1f4d8f24dc701
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:13 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -10,22 +10,15 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, substrate, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-      mkGoTool = (import "${substrate}/lib/go-tool.nix").mkGoTool;
-    in {
-      packages.default = mkGoTool pkgs {
-        pname = "akeyless-ui-webhook-builder";
-        version = "0.0.0-dev";
-        src = self;
-        vendorHash = "sha256-Dec/fOBBnyIBP4myS62koEBFXTxGrRkYa724qmuHFYE=";
-        description = "CLI tool to convert Chrome DevTools recordings into Akeyless custom rotator webhook scripts";
-        homepage = "https://github.com/pleme-io/akeyless-ui-webhook-builder";
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ go gopls gotools ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "go";
+    builder = "tool";
+    pname = "akeyless-ui-webhook-builder";
+    vendorHash = "sha256-Dec/fOBBnyIBP4myS62koEBFXTxGrRkYa724qmuHFYE=";
+    description = "CLI tool to convert Chrome DevTools recordings into Akeyless custom rotator webhook scripts";
+    homepage = "https://github.com/pleme-io/akeyless-ui-webhook-builder";
+  };
 }
